42c46a20c7fc598ac68846f84e9f3c4128d6d4dd
dfloresca <dfloresca@gmail.com>
2023-10-11 18:56:39 -0700

    feat: created index.html, main.js and main.css, built out boiler plate and declared some global variables and some basic css formatting

---
+++ js/main.js
@@ -0,0 +1,7 @@
+console.log('linked');
+
+const result = document.querySelector('#result');
+const addbtn = document.querySelector('#add');
+const minusbtn = document.querySelector('#minus');
+const incrementor = document.querySelector('#incrementor');
+
